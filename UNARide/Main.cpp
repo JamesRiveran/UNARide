8390bba42647a612a8792f7f17c3693e3e59e4aa
James Rivera <james.rivera.nunez@gmail.com>
2024-09-16 00:23:41 -0600

    [FEAT] CheckBox
    A checkbox was created to select the type of algorithm that the user wants to use

--- UNARide/Main.cpp
+++ UNARide/Main.cpp
@@ -17,6 +17,35 @@ int main() {
         static_cast<float>(window.getSize().y) / mapTexture.getSize().y
     );
 
+    sf::RectangleShape dijkstraCheckBox(sf::Vector2f(20.f, 20.f));
+    dijkstraCheckBox.setPosition(20.f, 20.f);
+    dijkstraCheckBox.setFillColor(sf::Color::White);
+    dijkstraCheckBox.setOutlineColor(sf::Color::Black);
+    dijkstraCheckBox.setOutlineThickness(2.f);
+
+    sf::RectangleShape floydCheckBox(sf::Vector2f(20.f, 20.f));
+    floydCheckBox.setPosition(20.f, 60.f);
+    floydCheckBox.setFillColor(sf::Color::White);
+    floydCheckBox.setOutlineColor(sf::Color::Black);
+    floydCheckBox.setOutlineThickness(2.f);
+
+    sf::Font font;
+    if (!font.loadFromFile("../arial.ttf")) {
+        std::cerr << "Failed to load the font." << std::endl;
+        return 1;
+    }
+
+    sf::Text dijkstraText("Dijkstra", font, 20);
+    dijkstraText.setPosition(50.f, 20.f);
+    dijkstraText.setFillColor(sf::Color::Black);
+
+    sf::Text floydText("Floyd-Warshall", font, 20);
+    floydText.setPosition(50.f, 60.f);
+    floydText.setFillColor(sf::Color::Black);
+
+    bool isDijkstraSelected = true;
+    bool isFloydSelected = false;
+
     sf::Texture carTexture;
     if (!carTexture.loadFromFile("../car.png")) {
         std::cerr << "Failed to load the car image." << std::endl;
@@ -24,14 +53,14 @@ int main() {
     }
 
     sf::Sprite carSprite(carTexture);
-    carSprite.setScale(0.1f, 0.1f);
+    carSprite.setScale(0.2f, 0.2f);
 
     Map map;
     map.loadNodesAndStreets("Nodes.txt");
 
     float speed = 75.0f;
     sf::Clock clock;
-    std::size_t startNode = 9, endNode = 39;
+    std::size_t startNode = 9, endNode = 35;
     std::vector<std::size_t> path = map.dijkstra(startNode, endNode);
     carSprite.setPosition(map.getNodes()[startNode].getPosition());
 
@@ -43,6 +72,35 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+
+            if (event.type == sf::Event::Resized) {
+                dijkstraCheckBox.setPosition(20.f, 20.f);
+                floydCheckBox.setPosition(20.f, 60.f);
+                dijkstraText.setPosition(50.f, 20.f);
+                floydText.setPosition(50.f, 60.f);
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+                    if (dijkstraCheckBox.getGlobalBounds().contains(mousePos)) {
+                        isDijkstraSelected = true;
+                        isFloydSelected = false;
+
+                        path = map.dijkstra(startNode, endNode);
+                        currentNodeInPath = 0;
+                        carSprite.setPosition(map.getNodes()[startNode].getPosition());
+                    }
+
+                    if (floydCheckBox.getGlobalBounds().contains(mousePos)) {
+                        isDijkstraSelected = false;
+                        isFloydSelected = true;
+
+                        // path = map.floydWarshall(startNode, endNode);
+                    }
+                }
             }
         }
 
@@ -76,9 +134,24 @@ int main() {
         }
 
         window.clear();
-        window.draw(mapSprite);
-        map.draw(window);
-        window.draw(carSprite);
+        window.draw(mapSprite);  
+        map.draw(window);        
+        window.draw(carSprite);  
+
+        window.draw(dijkstraCheckBox);
+        window.draw(floydCheckBox);
+        window.draw(dijkstraText);
+        window.draw(floydText);
+
+        if (isDijkstraSelected) {
+            dijkstraCheckBox.setFillColor(sf::Color::Green);
+            floydCheckBox.setFillColor(sf::Color::White);
+        }
+        else if (isFloydSelected) {
+            floydCheckBox.setFillColor(sf::Color::Green);
+            dijkstraCheckBox.setFillColor(sf::Color::White);
+        }
+
         window.display();
     }
 
